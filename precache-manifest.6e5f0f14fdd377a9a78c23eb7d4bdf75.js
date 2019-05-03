997d2e57fd1f04471db2151fd27d0a84b7f4737a
Alejandro Garzón <carlos.garzon@uao.edu.co>
2019-05-03 16:00:55 -0500

    init

---
+++ precache-manifest.6e5f0f14fdd377a9a78c23eb7d4bdf75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87f4e373ba835af6841bde29660681be",
+    "url": "/Pokemon/index.html"
+  },
+  {
+    "revision": "d6ed395cb3b519c53c97",
+    "url": "/Pokemon/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "bcd0c9e0f9de63042848",
+    "url": "/Pokemon/static/css/main.d24a69e9.chunk.css"
+  },
+  {
+    "revision": "d6ed395cb3b519c53c97",
+    "url": "/Pokemon/static/js/2.c83cbc8e.chunk.js"
+  },
+  {
+    "revision": "bcd0c9e0f9de63042848",
+    "url": "/Pokemon/static/js/main.8607e0d3.chunk.js"
+  },
+  {
+    "revision": "b733a247610cfe94ac5e",
+    "url": "/Pokemon/static/js/runtime~main.7b3eed47.js"
+  }
+]);
